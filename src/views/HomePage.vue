9dd2361a87a66bc876e2d7e2427de32d5aca7657
Taylor Smith <smith.taylord@gmail.com>
2025-05-17 15:26:50 -0600

    Theme Selector MVP

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,7 +5,7 @@
         <ion-text color="dark-green">
           <h1 class="home-title">Bingo in the Wild</h1>
         </ion-text>
-        <ion-button shape="round" size="large" color="salmon">
+        <ion-button shape="round" size="large" color="salmon" @click="openThemeSelectorModal">
           <ion-icon slot="start" :icon="trophy" />
           <span>Choose a Theme</span>
         </ion-button>
@@ -15,8 +15,30 @@
 </template>
 
 <script setup lang="ts">
-import {IonContent, IonHeader, IonPage, IonTitle, IonIcon} from '@ionic/vue';
+import {IonContent, IonPage, IonIcon, modalController, IonButton, IonText} from '@ionic/vue';
 import {trophy} from "ionicons/icons";
+import ThemeSelectorModal from "@/views/ThemeSelectorModal.vue";
+
+const openThemeSelectorModal = async () => {
+  const modal = await modalController.create({
+    component: ThemeSelectorModal,
+  });
+
+  await modal.present();
+
+  const { data, role } = await modal.onWillDismiss();
+
+  console.log('[data - This is the value]', data)
+  console.log('[role - this is going to either be cancel, or select]', role)
+
+  if (role === 'confirm') {
+    console.log("Confirmed")
+    // TODO This needs to then route to the Bingo Page with that theme selected
+    // So role + some sort of ID needs to be passed back
+  }
+};
+
+
 </script>
 
 <style scoped>
---
+++ src/views/ThemeSelectorModal.vue
@@ -0,0 +1,50 @@
+<template>
+  <ion-header>
+    <ion-toolbar color="salmon">
+      <ion-buttons slot="start">
+        <ion-button color="light" @click="cancel">Cancel</ion-button>
+      </ion-buttons>
+      <ion-title>
+        Select a Theme
+      </ion-title>
+    </ion-toolbar>
+    <ion-toolbar color="white" class="ion-padding-top">
+      <ion-searchbar></ion-searchbar>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding" color="white">
+    <ion-list lines="inset">
+      <ion-item color="white" button detail="false" v-for="theme in mockThemes" :key="theme">
+        <ion-label color="dark-green" @click="select(theme)">{{ theme }}</ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-content>
+</template>
+
+<script lang="ts" setup>
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonItem,
+  modalController,
+  IonSearchbar,
+  IonList,
+  IonLabel
+} from '@ionic/vue';
+
+const cancel = () => modalController.dismiss(null, 'cancel');
+const select = (value: string) => {
+  console.log(value)
+  modalController.dismiss(value, 'select')
+};
+
+const mockThemes = ['Hockey', 'Baseball', 'Boise Green Belt', 'General People Watching', 'Brewery People Watching'];
+</script>
+
+<style scoped>
+
+</style>
