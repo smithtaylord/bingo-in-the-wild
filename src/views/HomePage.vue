8dd995228cdf75e107a12da1fe7f009ecd57428c
Taylor Smith <smith.taylord@gmail.com>
2025-05-17 13:47:22 -0600

    MVP HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,28 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <ion-content class="ion-padding" fullscreen>
+      <div class="d-flex flex-column justify-content-center align-items-center h-100">
+        <ion-text color="dark-green">
+          <h1 class="home-title">Bingo in the Wild</h1>
+        </ion-text>
+        <ion-button shape="round" size="large" color="salmon">
+          <ion-icon slot="start" :icon="trophy" />
+          <span>Choose a Theme</span>
+        </ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {IonContent, IonHeader, IonPage, IonTitle, IonIcon} from '@ionic/vue';
+import {trophy} from "ionicons/icons";
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.home-title {
+  font-weight: bolder;
+  font-size: 30pt;
+  margin-bottom: 50px;
 }
 </style>
